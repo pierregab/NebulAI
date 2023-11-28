1797ac2833c4687a9baa8291b36f43a5a22b0438
Bibal Sobeaux Pierre Gabriel <36329132+pierregab@users.noreply.github.com>
2023-11-28 23:19:59 +0100

    Create main_training.py

---
+++ Training/main_training.py
@@ -0,0 +1,16 @@
+import data_retrieval as dr
+import nebula_image_downloader as nid
+import negative_image_downloader as nidr
+import dataset_exporter as de
+import model_training as mt
+
+dr.retrieve_dataset()
+images = nid.load_and_preprocess_images_parallel('StDr.csv')
+nid.clear_astropy_cache()
+
+not_images = nidr.download_negative_samples()
+nidr.clear_astropy_cache()
+
+de.export_dataset(images, not_images)
+
+history = mt.run_training(tune_hyperparams=True)
